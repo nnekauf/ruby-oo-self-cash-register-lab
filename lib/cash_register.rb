045cad4dac576662926cd5a0f5273ae5e22b1b2a
Nneka Ufumaka <ufumakanneka@gmail.com>
2021-01-24 01:32:02 -0500

    commit tree 985469bb920f64fc3a76087b98122dee23c64c53

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,1 +1,42 @@
 
+class CashRegister
+    
+    attr_accessor :discount, :total, :items
+    def initialize  (discount= 0)
+        @total = 0
+        @discount = discount
+        @items = []
+        
+        
+        
+    end
+
+    
+
+    def add_item(item, price, quantity = 1)
+        quantity.times do
+         @items << item
+        end
+        @last_total = @total
+       @total += price*quantity
+    end
+
+    def apply_discount
+       
+        if @discount == 0
+            "There is no discount to apply."
+        else
+            @total = @total - @total * @discount.to_i/100
+       "After the discount, the total comes to $#{@total}."
+        end
+
+    end
+    
+    def void_last_transaction
+        @total = @last_total
+    end
+
+        
+    
+
+end
